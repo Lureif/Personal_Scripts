e41fb48b33053da4c8b2fec14f93e1c52ee3efbe
Lureif <48881481+Lureif@users.noreply.github.com>
2020-03-25 11:19:40 +0100

    Delete init-theme-prompt.el

--- init-theme-prompt.el
+++
@@ -1,7 +0,0 @@
-(defun select-theme ()
-  "Asks the user if they want to load a light theme or a dark theme"
-  (if (y-or-n-p "Load light theme?")
-      (load-theme 'gruvbox-light-soft t)
-    (load-theme 'tango-dark t)))
-
-(select-theme)
